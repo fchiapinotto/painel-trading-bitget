634aac7f7691f33972a3acfecc30217a11be3c60
fchiapinotto <chiapinotto@gmail.com>
2025-04-09 21:59:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.markdown("""
     .card-preco {
         font-size: 52px;
         font-weight: bold;
-        margin-bottom: 10px;
+        margin-bottom: 8px;
     }
     .card-var {
         font-size: 22px;
@@ -33,7 +33,7 @@ st.markdown("""
         padding: 5px 10px;
         border-radius: 6px;
         display: inline-block;
-        margin-bottom: 10px;
+        margin-bottom: 18px;
 
     }
     .var-up { color: green; }
